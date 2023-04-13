cff5f56b7fe9454c9b2c6a10a2fcab4ce1184fd0
SvySor <svysor@gmail.com>
2023-04-13 09:21:23 +0300

    Seminar 01 added

---
+++ Seminar01/java1oop/src/main/java/org/example/Cat.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Cat {
+    private String name;
+    private String color;
+    private Integer age;
+
+}
---
+++ Seminar01/java1oop/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+    /*    Cat cat1 = new Cat();
+        cat1.name = "Barsik";
+        cat1.color = "Redhair";
+        cat1.age = 4;
+        System.out.println("Cat1 name: " + cat1.name + ", color: " + cat1.color + ", age: " + cat1.age);*/
+    }
+}
